4f5cefd048979d97f6b368ed7e7bef7dc6ccf02b
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 19:50:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
-require 'pry
-'
+require 'pry'
 def key_for_min_value(name_hash)
   if name_hash != {}
     valuesArr = []
