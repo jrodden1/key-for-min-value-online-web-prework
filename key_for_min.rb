76a29fc7c04845c3203000d5110d85d581ea5124
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 20:46:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,9 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 require 'pry'
 def key_for_min_value(name_hash)
+  min_num = 0
+  output = name_hash
+  
   if name_hash != {}
     valuesArr = []
   #This is going to dump all my numbers into an array that I'll iterate over
@@ -11,46 +14,12 @@ def key_for_min_value(name_hash)
       valuesArr << num
     end
 
-    potential_num
-    min_num
-    notThisNumber = []
-
-    valuesArr.each.with_index do |num, index|
-      potential_num = num
-
-      if notThisNumber.include?(potential_num) == false
-
-        valuesArr.each.with_index do |num, index|
-        next_num = index + 1
-
-         if potential_num > valuesArr[next_num]
-           notThisNumber << potential_num
-           puts "#{potential_num} was added to notThisNumber array"
-         else
-           min_num = potential_num
-           puts "#{potential_num} was added as the min_num"
-         end
-         binding.pry
-         #do nothing
-       end
-      else
-        puts "its not this number (#{potential_num})"  #else, skip this number
+    min_num = valuesArr.min
+    name_hash.each do |name, num|
+      if num == min_num
+        output = name
       end
-
     end
-
-    key_name = ""
-      name_hash.each do |name, num|
-        if num == min_num
-          key_name = name
-          puts key_name
-        end
-      end
-      binding.pry
-  else
-    key_name = nil
   end
-  puts key_name
-
-  key_name
+  output
 end
