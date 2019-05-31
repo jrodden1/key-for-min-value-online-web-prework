1e26240bef70111d593e3e15487e9ea18cd49728
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-31 19:47:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,32 +4,35 @@
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  valuesArr = []
+  if name_hash != {}
+    valuesArr = []
 
-#This is going to dump all my numbers into an array that I'll iterate over
-  name_hash.each do |name, num|
-    valuesArr << num
-  end
-
-  potential_num = nil
-  min_num = nil
-  notThisNumber = []
-  valuesArr.each.with_index do |num, index|
-    potential_num = num
-    if notThisNumber.include?(potential_num) == false
-      valuesArray.each.with_index do |num, index|
-      next_num = index + 1
-      potential_num > next_num ? notThisNumber << potential_num : min_num = potential_num
-      end
-      #else, skip this number
-    end
-  end
-
-  key_name = ""
+  #This is going to dump all my numbers into an array that I'll iterate over
     name_hash.each do |name, num|
-      if num == min_num
-        key_name = name
+      valuesArr << num
+    end
+
+    potential_num = nil
+    min_num = nil
+    notThisNumber = []
+    valuesArr.each.with_index do |num, index|
+      potential_num = num
+      if notThisNumber.include?(potential_num) == false
+        valuesArray.each.with_index do |num, index|
+        next_num = index + 1
+        potential_num > next_num ? notThisNumber << potential_num : min_num = potential_num
+        end
+        #else, skip this number
       end
     end
-  key_name
+
+    key_name = ""
+      name_hash.each do |name, num|
+        if num == min_num
+          key_name = name
+        end
+      end
+    key_name
+  else
+    {}  
 end
